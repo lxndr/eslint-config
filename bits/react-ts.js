d9261e2310879d7f1264246b822bf37e09da6b6e
Alexander Burobin <lxndr87i@gmail.com>
2020-05-30 16:17:22 +0300

    tweek @typescript-eslint/naming-convention for react

--- bits/react-ts.js
+++ bits/react-ts.js
@@ -4,5 +4,33 @@ module.exports = {
     'react/jsx-filename-extension': ['error', {
       extensions: ['.tsx'],
     }],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'default',
+        format: ['camelCase'],
+        leadingUnderscore: 'forbid',
+        trailingUnderscore: 'forbid',
+      },
+      {
+        selector: 'variable',
+        format: ['camelCase', 'UPPER_CASE'],
+      },
+      {
+        selector: 'memberLike',
+        modifiers: ['private'],
+        format: ['camelCase'],
+        leadingUnderscore: 'require',
+      },
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'variable',
+        types: ['function'],
+        format: ['PascalCase'],
+      },
+    ],
   },
 }
